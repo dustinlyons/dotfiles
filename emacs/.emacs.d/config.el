bea28739fabfb1ab183b1fc3d035ae0f442affa5
Dustin Lyons <hello@dustinlyons.co>
2021-07-27 15:30:11 -0400

    Adding Org ELPA to .el file, not just babel-org

--- emacs/.emacs.d/config.el
+++ emacs/.emacs.d/config.el
@@ -6,6 +6,8 @@
 
 (unless (assoc-default "melpa" package-archives)
   (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t))
+(unless (assoc-default "org" package-archives)
+  (add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t))
 (unless (assoc-default "nongnu" package-archives)
   (add-to-list 'package-archives '("nongnu" . "https://elpa.nongnu.org/nongnu/") t))
 
